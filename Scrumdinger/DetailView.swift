623a500bf6db025b1f68390cc2e8d60a47237ccf
hajinopro <hajinopro@gmail.com>
2022-05-24 11:12:11 +0900

    Updating App Data

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -47,6 +47,21 @@ struct DetailView: View {
             } header: {
                 Text("Attendees")
             }
+            
+            Section {
+                if scrum.history.isEmpty {
+                    Label("No meeting yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history) { history in
+                    HStack {
+                        Image(systemName: "calendar")
+                        Text(history.date, style: .date)
+                    }
+                }
+            } header: {
+                Text("History")
+            }
+
         }
         .navigationTitle(scrum.title)
         .toolbar {
